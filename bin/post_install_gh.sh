23586f6a4a6605aa84264aaec4add52e4c70fd1a
morgan <morgan.sziraki@gmail.com>
2025-11-30 00:25:39 +0000

    adding a handler

---
+++ bin/post_install_gh.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+: <<DOCXX
+Users will be redirected to this URL after installing your GitHub App to complete additional setup.
+https://github.com/morganism/github_webhook_app
+Author: morgan@morganism.dev
+Date: Sun Nov 30 00:23:16 GMT 2025
+DOCXX
+
+echo "ok"
