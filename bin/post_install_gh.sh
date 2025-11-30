47c293062ac4e328558c0075b4c99ac127e5adfb
morgan <morgan.sziraki@gmail.com>
2025-11-30 00:57:21 +0000

    add a lil

--- bin/post_install_gh.sh
+++ bin/post_install_gh.sh
@@ -4,6 +4,7 @@ Users will be redirected to this URL after installing your GitHub App to complet
 https://github.com/morganism/github_webhook_app
 Author: morgan@morganism.dev
 Date: Sun Nov 30 00:23:16 GMT 2025
+a8a9446fb816df889e5716f47922fc83d9691dd1
 DOCXX
 
 echo "ok"
